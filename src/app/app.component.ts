34f7ba584f05252195f22d5c6dbfd56b90f5ef7d
Harshal <harshalghate3131@gmail.com>
2021-09-01 21:32:43 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Demo';
+  public value1:string="";
+  public MyImg="";
+  public Accept(value:string)
+  {
+    this.value1=value;
+    this.value1="Happy Birthday "+this.value1;
+    this.MyImg="assets/a.jpg";
+  }
 }
+//<img [src]="MyImg" width="200px">
