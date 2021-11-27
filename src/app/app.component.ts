ee68fa6648dc7a2e8b03ec0a7cc9a35c53fbb35d
Harshal <harshalghate3131@gmail.com>
2021-11-27 23:26:45 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,16 +8,22 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 export class AppComponent {
   title = 'Demo';
   public value1:string="";
-  public MyImg="";
+  public MyImg=new Array(12);
+  public randomImg="";
   public Accept(value:string)
   {
     this.value1=value;
     this.value1="Happy Birthday "+this.value1;
-    this.MyImg="assets/a.jpg";
+    //this.MyImg="assets/a.jpg";
   }
   @ViewChild('audioOption')
   audioPlayerRef!: ElementRef;
-
+  Random()
+  {
+    
+      this.MyImg = ["assets/h1.jpg","assets/h2.jpg","assets/h3.jpg","assets/h4.jpg","assets/h5.jpg","assets/h6.jpg","assets/h7.jpg","assets/h8.jpg","assets/h9.jpg","assets/h10.jpg","assets/h11.jpg","assets/h12.jpg"];
+      this.randomImg = this.MyImg[Math.floor(Math.random() * this.MyImg.length)];
+  }  
   onAudioPlay()
   {
     this.audioPlayerRef.nativeElement.play();
